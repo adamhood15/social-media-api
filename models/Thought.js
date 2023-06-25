d49f5aa6150434253371ca2220872b35fa043e40
Adam Hood <adamhood15@gmail.com>
2023-06-25 13:15:57 -0500

    more laptop changes

--- models/Thought.js
+++ models/Thought.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
 
 //Schema used to create a Thought model
 
@@ -20,7 +21,7 @@ const thoughtSchema = new Schema(
             type: String,
             required: true
         },
-        //reactions: [reactionSchema]
+        reactions: [reactionSchema]
     },
     {
         toJSON: {
